e11af0613e04f556ac501ac682c3bea4c69552e7
Molo Munyansanga <molomunyansanga@gmail.com>
2025-12-09 21:09:06 +0200

    Add buyer paying route

--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -20,5 +20,27 @@ router.post("/transactions", authMiddleware, requireRole("BUYER"), async (reques
         return response.sendStatus(400);
     }
 });
+// Simulate buyer paying
+router.post("/transactions/:id/pay", authMiddleware, requireRole("BUYER"), async (req, res) => {
+    const transaction = await Transaction.findById(req.params.id);
+
+    if (!transaction) return res.status(404).json({ message: "Transaction not found" });
+
+    // Ensure buyer calling this endpoint owns this transaction
+    if (transaction.buyerId.toString() !== req.user.id) {
+      return res.status(403).json({ message: "Not your transaction" });
+    }
+
+    // Ensure state = PENDING_PAYMENT
+    if (transaction.state !== "PENDING_PAYMENT") {
+      return res.status(400).json({ message: "Payment already made" });
+    }
+
+    // Change to HOLDING
+    transaction.state = "HOLDING";
+    await transaction.save();
+
+    return res.json({ message: "Payment successful", transaction });
+});
 
 export default router;
