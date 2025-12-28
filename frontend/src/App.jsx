4bfd65b793bb47614634b55787d726ecb3352b62
Molo Munyansanga <molomunyansanga@gmail.com>
2025-12-28 22:40:58 +0200

    Add router connections

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,39 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const token = localStorage.getItem("token")
+      if (token) {
+        try {
+          const res = await axios.get("/auth/me", {
+            headers: {Authorization: `Bearer ${token}`}
+          })
+          setUser(res.data)
+        } catch (error) {
+          setError("Failed to fetch user data")
+          localStorage.removeItem("token")
+        }
+      }
+    }
+    fetchUser()
+  }, [])
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   )
